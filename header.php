4e4421fb5903fd2cee1128485d6795703aa5b0c0
Harry Atkins <harryatkins@me.com>
2024-02-14 11:51:16 +0000

    Dark mode per slide on hero slider

--- header.php
+++ header.php
@@ -1,83 +1,93 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo('charset')?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?php wp_head(); ?>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-        <!-- <div class="logoload">
+
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <!-- <div class="logoload">
             <div class="animation-logo-container">
                 <img src="<?php echo get_theme_file_uri('/assets/images/Sugaar_soft_texture.svg') ?>">
             </div>
         </div> -->
-    </head>
-    <body <?php body_class(); ?>>
-        <header class="header--container desktop">
-                <?php 
-                if( has_nav_menu( 'headerMenuLocation1' ) ) {
-                    wp_nav_menu( array( 'theme_location' => 'headerMenuLocation1') );
-                } else {
-                    echo '<div></div>';
-                }
-                ?>
+</head>
 
-            <div class="header--logo--container">
-                <a href="<?php echo site_url();?>">
-                    <img class="header--logo" src="<?php echo get_theme_file_uri('/assets/images/dragon_logo.svg') ?>"/>
-                </a>
-            </div>
+<body <?php body_class(); ?>>
+    <header class="header--container desktop">
+        <?php
+        if (has_nav_menu('headerMenuLocation1')) {
+            wp_nav_menu(array('theme_location' => 'headerMenuLocation1'));
+        } else {
+            echo '<div></div>';
+        }
+        ?>
 
-            <div class="menu-right-menu-container">
-                <ul id="menu-right-menu" class="menu">
-                    <li class="menu-item">
-                        <a data-open-popup href="#">Menu</a>
-                    </li>
-                    <li class="menu-item">
-                        <a data-open-reservations href="#">Reservations</a>
-                    </li>
-                </ul>
-            </div>
-        </header> 
+        <div class="header--logo--container">
+            <a href="<?php echo site_url(); ?>">
+                <img class="header--logo" src="<?php echo get_theme_file_uri('/assets/images/dragon_logo.svg') ?>" />
+                <img class="header--logo dark" src="<?php echo get_theme_file_uri('/assets/images/dragon_logo_black.svg') ?>" />
+            </a>
+        </div>
 
-        <header class="header--container mobile">
-            <div id="mobile-menu-icon">
-                <div class="close-icon-container">
+        <div class="menu-right-menu-container">
+            <ul id="menu-right-menu" class="menu">
+                <li class="menu-item">
+                    <a data-open-popup href="#">Menu</a>
+                </li>
+                <li class="menu-item">
+                    <a data-open-reservations href="#">Reservations</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+
+    <header class="header--container mobile">
+        <div id="mobile-menu-icon">
+            <div class="close-icon-container">
                 <div class="close-icon">
                 </div>
-                </div>
             </div>
-        </header>
-
-        <div class="mobile-menu-drawer">
-                <?php 
-                if( has_nav_menu( 'headerMenuLocation1' ) ) {
-                    wp_nav_menu( array( 'theme_location' => 'headerMenuLocation1', 'walker' => new Add_Div_Walker ));
-                } else {
-                    echo '<div></div>';
-                }
-                ?>
-                <div class="menu-right-menu-container">
-                <ul id="menu-right-menu" class="menu">
-                    <li class="menu-item">
-                        <a data-open-popup href="#">Menu</a>
-                    </li>
-                    <li class="menu-item">
-                        <a data-open-reservations href="#">Reservations</a>
-                    </li>
-                </ul>
-            </div>
-                <div class="menu-info-container">
-                        <div style="margin-bottom: 20px">
-                            <a href="https://maps.app.goo.gl/RRNZumJEku5gXJmD9" target="_blank"><p>5 Rue Gozlin<br>Saint-Germain-Des-Prés<br>6eme — Paris</p></a>
-                        </div>
-                        <div>
-                            <a target="_blank" href="tel:+33143265186"><p>+33 1 43 26 51 86</p></a>
-                            <a target="_blank" href="mailto:sugaar@sugaarparis.com"><p>sugaar@sugaarparis.com</p></a>
-                            <a target="_blank" href="https://www.instagram.com/sugaarparis"><p>Instagram</p></a>
-                        </div>
-                        <div class="logo">
-                            <img src="<?php echo get_theme_file_uri('/assets/images/logo_black.png') ?>"/>
-                        </div>
-                </div>
         </div>
-  
+    </header>
+
+    <div class="mobile-menu-drawer">
+        <?php
+        if (has_nav_menu('headerMenuLocation1')) {
+            wp_nav_menu(array('theme_location' => 'headerMenuLocation1', 'walker' => new Add_Div_Walker));
+        } else {
+            echo '<div></div>';
+        }
+        ?>
+        <div class="menu-right-menu-container">
+            <ul id="menu-right-menu" class="menu">
+                <li class="menu-item">
+                    <a data-open-popup href="#">Menu</a>
+                </li>
+                <li class="menu-item">
+                    <a data-open-reservations href="#">Reservations</a>
+                </li>
+            </ul>
+        </div>
+        <div class="menu-info-container">
+            <div style="margin-bottom: 20px">
+                <a href="https://maps.app.goo.gl/RRNZumJEku5gXJmD9" target="_blank">
+                    <p>5 Rue Gozlin<br>Saint-Germain-Des-Prés<br>6eme — Paris</p>
+                </a>
+            </div>
+            <div>
+                <a target="_blank" href="tel:+33143265186">
+                    <p>+33 1 43 26 51 86</p>
+                </a>
+                <a target="_blank" href="mailto:sugaar@sugaarparis.com">
+                    <p>sugaar@sugaarparis.com</p>
+                </a>
+                <a target="_blank" href="https://www.instagram.com/sugaarparis">
+                    <p>Instagram</p>
+                </a>
+            </div>
+            <div class="logo">
+                <img src="<?php echo get_theme_file_uri('/assets/images/logo_black.png') ?>" />
+            </div>
+        </div>
+    </div>
--- src/modules/HeroSlider.js
+++ src/modules/HeroSlider.js
@@ -1,50 +1,67 @@
 class HeroSlider {
-    constructor() {
-        this.currentSlide = 0
-        this.slides = document.querySelectorAll('.hero-slider__slide')
+  constructor() {
+    this.currentSlide = 0;
+    this.slides = document.querySelectorAll(".hero-slider__slide");
 
-        this.init()
-    }
-    init() {
+    this.init();
+  }
+  init() {
+    document.querySelector(".hero-slider-arrow.next").addEventListener("click", () => this.nextSlide());
+    document.querySelector(".hero-slider-arrow.prev").addEventListener("click", () => this.prevSlide());
 
-        document.querySelector('.hero-slider-arrow.next').addEventListener('click', () => this.nextSlide())
-        document.querySelector('.hero-slider-arrow.prev').addEventListener('click', () => this.prevSlide())
-
-        let that = this
-        setInterval(function () {
-            if (that.currentSlide === (that.slides.length - 1)) {
-                that.slides[that.currentSlide].classList.add('fade')
-                that.currentSlide = 0
-                that.slides[that.currentSlide].classList.remove('fade')
-            } else {
-                that.slides[that.currentSlide].classList.add('fade')
-                that.currentSlide += 1
-                that.slides[that.currentSlide].classList.remove('fade')
-            }
-        }, 4000)
+    let that = this;
+    setInterval(function () {
+      if (that.currentSlide === that.slides.length - 1) {
+        that.slides[that.currentSlide].classList.add("fade");
+        that.currentSlide = 0;
+        that.slides[that.currentSlide].classList.remove("fade");
+      } else {
+        that.slides[that.currentSlide].classList.add("fade");
+        that.currentSlide += 1;
+        that.slides[that.currentSlide].classList.remove("fade");
+      }
+      let newTheme = that.slides[that.currentSlide].getAttribute("data-theme");
+      if (newTheme === "dark") {
+        document.body.classList.add("theme-dark");
+      } else {
+        document.body.classList.remove("theme-dark");
+      }
+    }, 4000);
+  }
+  nextSlide() {
+    if (this.currentSlide === this.slides.length - 1) {
+      this.slides[this.currentSlide].classList.add("fade");
+      this.currentSlide = 0;
+      this.slides[this.currentSlide].classList.remove("fade");
+    } else {
+      this.slides[this.currentSlide].classList.add("fade");
+      this.currentSlide += 1;
+      this.slides[this.currentSlide].classList.remove("fade");
     }
-    nextSlide() {
-        if (this.currentSlide === (this.slides.length - 1)) {
-            this.slides[this.currentSlide].classList.add('fade')
-            this.currentSlide = 0
-            this.slides[this.currentSlide].classList.remove('fade')
-        } else {
-            this.slides[this.currentSlide].classList.add('fade')
-            this.currentSlide += 1
-            this.slides[this.currentSlide].classList.remove('fade')
-        }
+    let newTheme = this.slides[this.currentSlide].getAttribute("data-theme");
+    if (newTheme === "dark") {
+      document.body.classList.add("theme-dark");
+    } else {
+      document.body.classList.remove("theme-dark");
     }
-    prevSlide() {
-        if (this.currentSlide === 0) {
-            this.slides[0].classList.add('fade')
-            this.currentSlide = this.slides.length - 1
-            this.slides[this.slides.length - 1].classList.remove('fade')
-        } else {
-            this.slides[this.currentSlide].classList.add('fade')
-            this.currentSlide -= 1
-            this.slides[this.currentSlide].classList.remove('fade')
-        }
+  }
+  prevSlide() {
+    if (this.currentSlide === 0) {
+      this.slides[0].classList.add("fade");
+      this.currentSlide = this.slides.length - 1;
+      this.slides[this.slides.length - 1].classList.remove("fade");
+    } else {
+      this.slides[this.currentSlide].classList.add("fade");
+      this.currentSlide -= 1;
+      this.slides[this.currentSlide].classList.remove("fade");
     }
+    let newTheme = this.slides[this.currentSlide].getAttribute("data-theme");
+    if (newTheme === "dark") {
+      document.body.classList.add("theme-dark");
+    } else {
+      document.body.classList.remove("theme-dark");
+    }
+  }
 }
 
-export default HeroSlider
+export default HeroSlider;
